9e6562beaee10cc1ae647b8deff5098d3227eaa1
Jean-Emmanuel B <jeanemmanuel.baillat@gmail>
2015-03-01 17:07:22 +0100

    Affichage des salles disponibles après reherche & réservation possible sur clic après demande de confirmation

--- app/src/main/java/projetandroid/entmobile/Reservation.java
+++ app/src/main/java/projetandroid/entmobile/Reservation.java
@@ -1,10 +1,13 @@
 package projetandroid.entmobile;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.database.MatrixCursor;
 import android.os.Bundle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -23,6 +26,7 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.NumberPicker;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,14 +42,14 @@ public class Reservation extends ActionBarActivity implements View.OnClickListen
     private CharSequence titleBar;
     private CharSequence titleMenu;
     private Intent intent;
-    private Button boutonRechercheUE;
-    private Button boutonRechercheUE_new;
-    private Animation anim_panelRechercheUE_show;
-    private Animation anim_panelRechercheUE_hide;
-    private Animation anim_panelRechercheUE_results_show;
-    private Animation anim_panelRechercheUE_results_hide;
-    private LinearLayout panelRechercheUE;
-    private LinearLayout panelRechercheUE_results;
+    private Button boutonRecherche_salle;
+    private Button boutonRecherche_salle_new;
+    private Animation anim_panelRecherche_salle_show;
+    private Animation anim_panelRecherche_salle_hide;
+    private Animation anim_panelRecherche_salle_results_show;
+    private Animation anim_panelRecherche_salle_results_hide;
+    private LinearLayout panelRecherche_salle;
+    private LinearLayout panelRecherche_salle_results;
     private NumberPicker numberPicker_effectif;
     private TextView effectif_text;
     private NumberPicker numberPicker_duree_cours;
@@ -70,6 +74,15 @@ public class Reservation extends ActionBarActivity implements View.OnClickListen
     private String debut_cours;
     private int duree_cours;
     private int effectif;
+    private MatrixCursor matrixCursor;
+    private AlertDialog.Builder alertConfirmBuilder;
+    private AlertDialog alertConfirm;
+    private ListView lv_results;
+    private TextView lv_column_nom_salle;
+    private TextView lv_column_departement;
+    private TextView lv_column_capacite;
+    private TextView lv_column_horaire;
+    private LinearLayout lv_row;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -80,11 +93,11 @@ public class Reservation extends ActionBarActivity implements View.OnClickListen
         titleMenu = getResources().getString(R.string.label_menu);
 
         //Définition des éléments de la page
-        boutonRechercheUE = (Button)findViewById(R.id.boutonRechercheUE);
-        boutonRechercheUE_new = (Button)findViewById(R.id.boutonRechercheUE_new);
+        boutonRecherche_salle = (Button)findViewById(R.id.boutonRecherche_salle);
+        boutonRecherche_salle_new = (Button)findViewById(R.id.boutonRecherche_salle_new);
         boutonReinitialize = (Button)findViewById(R.id.boutonReinitialize);
-        panelRechercheUE = (LinearLayout)findViewById(R.id.panel_rechercheUE);
-        panelRechercheUE_results = (LinearLayout)findViewById(R.id.panel_rechercheUE_results);
+        panelRecherche_salle = (LinearLayout)findViewById(R.id.panel_recherche_salle);
+        panelRecherche_salle_results = (LinearLayout)findViewById(R.id.panel_recherche_salle_results);
         effectif_text = (TextView)findViewById(R.id.effectif_recherche);
         numberPicker_effectif = (NumberPicker)findViewById(R.id.effectif_recherche_number_picker);
         numberPicker_duree_cours = (NumberPicker)findViewById(R.id.duree_cours_recherche_number_picker);
@@ -96,22 +109,33 @@ public class Reservation extends ActionBarActivity implements View.OnClickListen
         nom_equipement_a = (Spinner)findViewById(R.id.nom_equipement_a);
         nom_equipement_b = (Spinner)findViewById(R.id.nom_equipement_b);
         horaire_debut_cours = (Spinner)findViewById(R.id.horaire_debut_cours);
+        lv_results = (ListView)findViewById(R.id.listview_recherche_results);
+        lv_column_nom_salle = (TextView)findViewById(R.id.colonne_nom_salle);
+        lv_column_departement = (TextView)findViewById(R.id.colonne_departement);
+        lv_column_capacite = (TextView)findViewById(R.id.colonne_capacite);
+        lv_column_horaire = (TextView)findViewById(R.id.colonne_horaire);
+        lv_row = (LinearLayout)findViewById(R.id.row_recherche_results);
 
 
         //Définition des éléments de sélection
         setDatePickerDialog();
         setNumberPicker();
 
+        //Définition de la boîte de dialogue de confirmation de réservation
+        alertConfirmBuilder = new AlertDialog.Builder(this);
+        initalertConfirmBuilder();
+        alertConfirm = alertConfirmBuilder.create();
+
         //Définition des animations
-        anim_panelRechercheUE_show = AnimationUtils.loadAnimation(this, R.anim.push_left_in);
-        anim_panelRechercheUE_hide = AnimationUtils.loadAnimation(this, R.anim.push_right_out);
-        anim_panelRechercheUE_results_show = AnimationUtils.loadAnimation(this, R.anim.push_right_in);
-        anim_panelRechercheUE_results_hide = AnimationUtils.loadAnimation(this, R.anim.push_left_out);
+        anim_panelRecherche_salle_show = AnimationUtils.loadAnimation(this, R.anim.push_left_in);
+        anim_panelRecherche_salle_hide = AnimationUtils.loadAnimation(this, R.anim.push_right_out);
+        anim_panelRecherche_salle_results_show = AnimationUtils.loadAnimation(this, R.anim.push_right_in);
+        anim_panelRecherche_salle_results_hide = AnimationUtils.loadAnimation(this, R.anim.push_left_out);
         anim_boutonCalendar = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
 
         //Définition des listeners
-        boutonRechercheUE.setOnClickListener(this);
-        boutonRechercheUE_new.setOnClickListener(this);
+        boutonRecherche_salle.setOnClickListener(this);
+        boutonRecherche_salle_new.setOnClickListener(this);
         boutonReinitialize.setOnClickListener(this);
         calendar.setOnClickListener(this);
         selection_setListner();
@@ -149,6 +173,68 @@ public class Reservation extends ActionBarActivity implements View.OnClickListen
         drawerListView.setOnItemClickListener(new DrawerItemClickListener());
     }
 
+    //Définition de l'affichage des résultats de la recherche
+    private void setListViewResults() {
+        // Définition des colonnes du tableau des résultats de la recherche
+        String[] columns = new String[] { "_id", "nom_salle", "departement", "capacite", "horaire" };
+        matrixCursor = new MatrixCursor(columns);
+        startManagingCursor(matrixCursor);
+        matrixCursor.addRow(new Object[] { 1,"SC 3005","Maths-info", "18", "09:00" });
+        matrixCursor.addRow(new Object[] { 2,"SC 3002","Maths-info", "20", "13:30" });
+
+        // Prendre les données de toutes les colonnes
+        String[] from = new String[] {"nom_salle", "departement", "capacite", "horaire"};
+
+        // Placer chaque donnée dans les colonnes respectives
+        int[] to = new int[] { R.id.colonne_nom_salle, R.id.colonne_departement, R.id.colonne_capacite, R.id.colonne_horaire};
+
+        // Définition de l'adapter pour remplir le tableau avec les résultats
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.row_listview, matrixCursor, from, to, 0);
+        lv_results = (ListView) findViewById(R.id.listview_recherche_results);
+        lv_results.setAdapter(adapter);
+        // Gestion des clics sur les lignes
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View container, int position, long id) {
+                //Gestion du clic de l'utilisateur sur une ligne de résultat
+                alertConfirm.show();
+            }
+        };
+
+    // Utilisation avec notre listview
+        lv_results.setOnItemClickListener(itemClickListener);
+    }
+
+    //Définition du contenu de la boîte de dialogue
+    private void initalertConfirmBuilder() {
+        alertConfirmBuilder.setTitle(R.string.alertReservationTitle);
+        String confirm_txt = getResources().getString(R.string.alertReservationMessage);
+        String nom_date = jour+"-"+(mois+1)+"-"+annee;
+        alertConfirmBuilder.setMessage("Confirmez-vous la réservation de la salle SC3005 le "+nom_date+" à 09:00 ?");
+        alertConfirmBuilder.setIcon(R.drawable.ic_action_time);
+
+        alertConfirmBuilder.setPositiveButton(R.string.oui, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                /* TODO */
+                /*Code à définir pour réserver une salle dans la bdd*/
+                reinitialize();
+                panelRecherche_salle_results.startAnimation(anim_panelRecherche_salle_results_hide);
+                panelRecherche_salle_results.setVisibility(View.INVISIBLE);
+                panelRecherche_salle.startAnimation(anim_panelRecherche_salle_show);
+                panelRecherche_salle.setVisibility(View.VISIBLE);
+                Toast.makeText(getApplicationContext(), R.string.alertConfirmToast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        alertConfirmBuilder.setNegativeButton(R.string.non, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+    }
+
+    //Gestion de la sélection des items dans les dropdown list
     private void selection_setListner() {
         type_salle.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -326,18 +412,21 @@ public class Reservation extends ActionBarActivity implements View.OnClickListen
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.boutonRechercheUE:
-                panelRechercheUE.startAnimation(anim_panelRechercheUE_hide);
-                panelRechercheUE.setVisibility(View.INVISIBLE);
-                panelRechercheUE_results.startAnimation(anim_panelRechercheUE_results_show);
-                panelRechercheUE_results.setVisibility(View.VISIBLE);
+            case R.id.boutonRecherche_salle:
+                setListViewResults();
+                panelRecherche_salle.startAnimation(anim_panelRecherche_salle_hide);
+                panelRecherche_salle.setVisibility(View.INVISIBLE);
+                panelRecherche_salle_results.startAnimation(anim_panelRecherche_salle_results_show);
+                panelRecherche_salle_results.setVisibility(View.VISIBLE);
+                //Code pour la requête SQL
                 break;
 
-            case R.id.boutonRechercheUE_new:
-                panelRechercheUE_results.startAnimation(anim_panelRechercheUE_results_hide);
-                panelRechercheUE_results.setVisibility(View.INVISIBLE);
-                panelRechercheUE.startAnimation(anim_panelRechercheUE_show);
-                panelRechercheUE.setVisibility(View.VISIBLE);
+            case R.id.boutonRecherche_salle_new:
+                reinitialize();
+                panelRecherche_salle_results.startAnimation(anim_panelRecherche_salle_results_hide);
+                panelRecherche_salle_results.setVisibility(View.INVISIBLE);
+                panelRecherche_salle.startAnimation(anim_panelRecherche_salle_show);
+                panelRecherche_salle.setVisibility(View.VISIBLE);
                 break;
 
             case R.id.boutonReinitialize:
